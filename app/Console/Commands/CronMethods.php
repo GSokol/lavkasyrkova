f20c43c6ab77c314a670bc7e4d906478b1720791
Romis <romis.nesmelov@gmail.com>
2019-12-21 15:07:17 +0300

    testing tastings informing

--- app/Console/Commands/CronMethods.php
+++ app/Console/Commands/CronMethods.php
@@ -43,10 +43,10 @@ class CronMethods extends Command
         foreach ($tastings as $tasting) {
             foreach ($tasting->office->users as $user) {
                 if ($user->send_mail) {
-                    $this->sendMessage($user->email,'auth.emails.tasting_informing', [
+                    $this->sendMessage($user->email, 'auth.emails.tasting_informing', [
                         'address' => $tasting->office->address,
                         'time' => date('d.m.Y',$tasting->time)
-                    ]);
+                    ], 'romis.nesmelov@gmail.com');
                 }
             }
             $tasting->informed = 1;
