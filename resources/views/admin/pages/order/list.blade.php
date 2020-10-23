8023aaa265cbd66407f7b19105219d55b7c3dc60
Vitaliy Artyukh <vitaliy.artyukh@gmail.com>
2020-10-23 15:15:00 +0300

    Feature #9 - fix admin order list

--- resources/views/admin/pages/order/list.blade.php
+++ resources/views/admin/pages/order/list.blade.php
@@ -12,27 +12,27 @@
                     <th class="text-center">Номер заказа</th>
                     <th class="text-center">Время создания</th>
                     <th class="text-center">Доставка</th>
-                    <th class="text-center">Дата дегустации</th>
+                    <!-- <th class="text-center">Дата дегустации</th> -->
                     <th class="text-center">Примечание</th>
-                    <th class="text-center">Состав</th>
+                    <th class="text-center col-md-3">Состав</th>
                     <th class="text-center">Статус заказа</th>
                     <th class="text-center">Стоимость</th>
                 </tr>
                 @foreach ($orders as $order)
                     <tr role="row" id="{{ 'order_'.$order->id }}">
                         <td class="text-center id">{{ $order->id }}</td>
-                        <td class="text-center"><a href="{{ route('admin.order', ['id' => $order->id]) }}">{{ $order->created_at->format('d.m.Y H:i:s') }}</a></td>
+                        <td class="text-center"><a href="{{ route('admin.order', ['id' => $order->id]) }}">{{ $order->created_at }}</a></td>
                         <td class="text-center">{{ $order->delivery_info }}</td>
-                        <td class="text-center">
-                            @if ($order->user->office_id > 2 && !$order->delivery && !$order->shop_id)
-                                @foreach($order->user->office->tastings as $tasting)
-                                    @if ($tasting->time > $order->created_at->timestamp)
-                                        {{ date('d.m.Y',$tasting->time) }}
-                                        @break
-                                    @endif
-                                @endforeach
-                            @endif
-                        </td>
+                        <!-- <td class="text-center">
+                            if ($order->user->office_id > 2 && !$order->delivery && !$order->shop_id)
+                                foreach($order->user->office->tastings as $tasting)
+                                    if ($tasting->time > $order->created_at->timestamp)
+                                        date('d.m.Y', tasting->time) }}
+                                        break
+                                    endif
+                                endforeach
+                            endif
+                        </td> -->
                         <td class="text-left">{{ $order->description }}</td>
                         <td class="text-left">@include('admin._order_content_block', ['order' => $order])</td>
                         <td class="text-center">
